de6b9bfa4edeed4d3dda4f22d087bdee882732e7
Elias <elias_coelho911@hotmail.com>
2025-09-11 16:08:54 -0300

    Initial commit

---
+++ ms-orders/src/main/kotlin/com/eliascoelho911/efood/ms_orders/MsOrdersApplication.kt
@@ -0,0 +1,11 @@
+package com.eliascoelho911.efood.ms_orders
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MsOrdersApplication
+
+fun main(args: Array<String>) {
+	runApplication<MsOrdersApplication>(*args)
+}
---
+++ ms-orders/src/test/kotlin/com/eliascoelho911/efood/ms_orders/MsOrdersApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.eliascoelho911.efood.ms_orders
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MsOrdersApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
