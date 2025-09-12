b8cb667e25003d7836eaf36bc8941d3eb0f48741
Elias <elias_coelho911@hotmail.com>
2025-09-12 12:24:18 -0300

    chore: Registra o MsOrders no Service Registry

--- ms-orders/src/main/kotlin/com/eliascoelho911/efood/ms_orders/MsOrdersApplication.kt
+++ ms-orders/src/main/kotlin/com/eliascoelho911/efood/ms_orders/MsOrdersApplication.kt
@@ -2,8 +2,10 @@ package com.eliascoelho911.efood.ms_orders
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
 @SpringBootApplication
+@EnableDiscoveryClient
 class MsOrdersApplication
 
 fun main(args: Array<String>) {
