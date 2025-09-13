fa610bce30c58bbca53057372968f392181c96c5
Elias <elias_coelho911@hotmail.com>
2025-09-13 18:52:21 -0300

    feat: Implement circuit breaker in the payment status request from the request order process

--- ms-orders/src/main/kotlin/com/eliascoelho911/efood/ms_orders/client/PaymentClient.kt
+++ ms-orders/src/main/kotlin/com/eliascoelho911/efood/ms_orders/client/PaymentClient.kt
@@ -5,7 +5,6 @@ import com.eliascoelho911.efood.ms_orders.dto.AuthorizationDto
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
 
 @FeignClient("ms-payments", configuration = [FeignConfig::class])
 interface PaymentClient {
